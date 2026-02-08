d79568fc3a84fe12710a3e5399f8ddbe301060f3
Misha <mishaunity28@gmail.com>
2026-02-08 13:08:05 +0300

    Spawn random animal

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+ public int moveSpeed = 5;
+
+ public GameObject food;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     float horizontalInput = Input.GetAxis("Horizontal");
+     transform.Translate(Vector3.right * moveSpeed * horizontalInput * Time.deltaTime);
+
+     if (transform.position.x > 15)
+     {
+         transform.position = new Vector3(15, 0, 0);
+     }
+
+     if (transform.position.x < -15)
+     {
+         transform.position = new Vector3(-15, 0, 0);
+     }
+
+     if (Input.GetKeyDown(KeyCode.Space))
+     {
+         Instantiate(food, transform.position, food.transform.rotation);
+     }
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animals;
+    public float spawnPositionZ=20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Vector3 spawnPosition = new Vector3(0,0,spawnPositionZ);
+            int randomAnimal = Random.Range(0, animals.Length);
+            Instantiate(animals[randomAnimal], spawnPosition, animals[randomAnimal].transform.rotation);
+        }
+
+    }
+}
