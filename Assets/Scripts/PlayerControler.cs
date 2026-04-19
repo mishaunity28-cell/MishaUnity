93639fe08c0a81aefe3e7f6289025f00628f2d4f
Maxwell the cat <mishaunity28@gmail.com>
2026-04-19 11:51:50 +0300

    Make player jump on ground

---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public float jumpForce;
+    public float gravityModifier;
+    private Rigidbody rigidbody;
+    private bool isOnGround = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+      Physics.gravity *= gravityModifier;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround == true)
+        {
+            rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.TryGetComponent(out Ground ground))
+        {
+            isOnGround = true;
+        }
+    }
+}
+    
